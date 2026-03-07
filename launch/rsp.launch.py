2417a77548e3c47aa8b23c717979bcc58e3d56d2
VB2404 <vrishank.rishi@gmail.com>
2026-03-07 17:46:45 +0530

    changes included to the launch file

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+
 from launch_ros.actions import Node
 
 import xacro
@@ -12,16 +13,25 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Launch argument
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # Get path to package
+    pkg_path = get_package_share_directory('my_bot')
+
+    # Path to xacro file
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+
+    # Process xacro
     robot_description_config = xacro.process_file(xacro_file).toxml()
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+
+    # Robot description parameter
+    params = {
+        'robot_description': robot_description_config,
+        'use_sim_time': use_sim_time
+    }
+
+    # Robot State Publisher
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,13 +39,30 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Joint State Publisher GUI
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
 
-    # Launch!
+    # RViz
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    )
+
     return LaunchDescription([
+
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
+            description='Use simulation time if true'
+        ),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        node_joint_state_publisher_gui,
+        node_rviz
+
     ])
