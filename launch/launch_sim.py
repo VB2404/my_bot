cb7095255b0e243ae3a941ddbaebce1da87c099d
VB2404 <vrishank.rishi@gmail.com>
2026-03-09 17:24:37 +0530

    Launch fileto launch gz ros2 and publisher

---
+++ launch/launch_sim.py
@@ -0,0 +1,43 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription,TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name="my_bot"
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]) , 
+        launch_arguments={'use_sim_time' : 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'),
+            'launch','gz_sim.launch.py'
+        )]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    spawn_entity = TimerAction(period=3.0, actions=[
+        Node(
+            package='ros_gz_sim',           
+            executable='create',
+            arguments=['-topic', '/robot_description', '-entity', 'my_bot'],  
+            output='screen'
+        )
+    ])
+    
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity
+    ])
